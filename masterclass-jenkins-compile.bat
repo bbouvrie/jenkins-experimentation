929a1b44ce783220dc61712f67e823433591e0be
bbouvrie <43411848+bbouvrie@users.noreply.github.com>
2018-09-24 09:06:32 +0200

    Removed superfluous paths

--- masterclass-jenkins-compile.bat
+++ masterclass-jenkins-compile.bat
@@ -5,11 +5,11 @@ echo Adding and changing dir to build
 mkdir build
 cd build
 echo Setting qmake compiler 
-"C:/Qt/5.11.1/mingw53_32/bin/qmake.exe"  "../src/all.pro" -spec win32-g++ "CONFIG+=qtquickcompiler"
+qmake  "../src/all.pro" -spec win32-g++ "CONFIG+=qtquickcompiler"
 echo Executing qmake
-"C:/Qt/Tools/mingw530_32/bin/mingw32-make.exe" "qmake_all"
+mingw32-make "qmake_all"
 echo executing make 
-"C:/Qt/Tools/mingw530_32/bin/mingw32-make.exe"
+mingw32-make
 echo done building
 
        
